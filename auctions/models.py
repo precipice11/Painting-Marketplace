672ac84af4f84264c4b26c65287ee7f4ba4c0387
precipice11 <focustwo11@gmail.com>
2024-12-09 15:12:09 +1100

    Add Models for Category, Auction_Listing, Bid, Comments

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,42 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Auction_Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    image = models.ImageField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="listings")
+
+    def __str__(self):
+        return self.title
+    
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    listing = models.ForeignKey(Auction_Listing, on_delete=models.CASCADE, related_name="bids")
+    bid_amount = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Bid by {self.user.username} for {self.listing.title}"
+    
+
+
+class Comment(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    listing = models.ForeignKey(Auction_Listing, on_delete=models.CASCADE, related_name="comments")
+    text=models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.listing.title}"
+
+
