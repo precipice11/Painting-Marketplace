a72dce63305a2514c170b0c88ed4f0ba52cdfd0e
precipice11 <focustwo11@gmail.com>
2024-12-09 15:37:25 +1100

    Add Active Listings page

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Category, Auction_Listing, Bid, Comment
+
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Auction_Listing)
+admin.site.register(Bid)
+admin.site.register(Comment)
